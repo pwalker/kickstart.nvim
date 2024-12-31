a86da936014bebbdbb27c19e531afdfa0c662c4c
Peter Walker <pwalker@nutshelllabs.io>
2024-12-31 10:38:39 -0600

    remove prettierd

--- init.lua
+++ init.lua
@@ -733,9 +733,9 @@ require('lazy').setup({
         -- python = { "isort", "black" },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
-        javascript = { 'prettierd', 'prettier', stop_after_first = true },
-        typescript = { 'prettierd', 'prettier', stop_after_first = true },
-        typescriptreact = { 'prettierd', 'prettier', stop_after_first = true },
+        javascript = { 'prettier', stop_after_first = true },
+        typescript = { 'prettier', stop_after_first = true },
+        typescriptreact = { 'prettier', stop_after_first = true },
         markdown = { 'prettier' },
       },
     },
