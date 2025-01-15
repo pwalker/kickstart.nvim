84b0df216189d411e5c18c73dc254c48ef73b82e
Peter Walker <peterw@suprafax.net>
2025-01-14 22:30:07 -0600

    enable indentscope

--- init.lua
+++ init.lua
@@ -760,6 +760,8 @@ require('lazy').setup({
       -- set use_icons to true if you have a Nerd Font
       statusline.setup { use_icons = vim.g.have_nerd_font }
 
+      require('mini.indentscope').setup()
+
       -- You can configure sections in the statusline by overriding their
       -- default behavior. For example, here we set the section for
       -- cursor location to LINE:COLUMN
